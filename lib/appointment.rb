2c82e114837edf99d9639893386c72f0e2094ca2
Dominic Chapman <dominicchapman@me.com>
2017-10-12 21:59:12 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,9 +1,11 @@
 class Appointment
 
-  attr_accessor :date
+  attr_accessor :date, :doctor
 
-  def initialize(date, doctor)
+  def initialize(date, the_doctor)
     @date = date
+    self.doctor = the_doctor
+    doctor.appointments << self
   end
 
 end
